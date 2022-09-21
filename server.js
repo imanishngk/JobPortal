d7032419a874c39860a1b5a3a7156127887b9245
imanishngk <imanishnagarkoti@gmail.com>
2022-09-21 16:32:47 +0545

    server created

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/mid", (req, res, next)=>{
+    res.status(200).send("middleware created")
+})
+
+app.listen(process.env.PORT, (data, err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(process.env.port);
+    console.log("listening...");
+  }
+});
